b71572aecf7a1e410ea73ab48caaee866c2188f5
VijayrajS <vijayrajshanmugarajt@gmail.com>
2025-03-20 19:43:24 -0400

    Added event ID validation - bug fixes

--- app/utils.py
+++ app/utils.py
@@ -14,6 +14,7 @@ def post_process(data):
     return jsonify(data)
 
 def is_valid_past_timestamp(timestamp:str):
+    timestamp = int(timestamp)
     current_timestamp = int(time.time())
     return 0 < timestamp < current_timestamp
     
@@ -23,11 +24,13 @@ def is_valid_asn(asn:str):
         isValid = True
 
         # checking for 32 bit int
-        if isinstance(asn, int) and 1 <= asn <= 4294967295:
+        if 1 <= asn <= 4294967295:
             # Checking if private/reserved ASN
             if 64512 <= asn <= 65534 or 4200000000 <= asn <= 4294967295:
                 isValid = False
-        
+        else:
+            isValid = False
+
         return isValid
     
     except ValueError:
@@ -39,9 +42,9 @@ def validate_moas(as_list):
         as_list_arr = as_list.split('_')
         for asn in as_list_arr:
             assert is_valid_asn(asn)  
-
-    except AssertionError:
-        err_str = "Invalid MOAS event. The right format for a MOAS event is moas-<unix_timestamp>-<asn1_asn2_...>"
+    
+    except Exception as e:
+        err_str = "Invalid MOAS event. The right format for a MOAS event is moas-<unix_timestamp>-<asn1_asn2_...>, with each asn being a valid AS number"
         raise ValidationError(err_str)
 
 def validate_submoas(as_list):
@@ -57,8 +60,8 @@ def validate_submoas(as_list):
         for asn in attacker_list:
             assert is_valid_asn(asn)
 
-    except AssertionError:
-        err_str = "Invalid SUBMOAS event. The right format for a SUBMOAS event is submoas-<unix_timestamp>-<victim-asn1_victim-asn2_...>=<attacker-asn1_attacker-asn2...>"
+    except Exception as e:
+        err_str = "Invalid SUBMOAS event. The right format for a SUBMOAS event is submoas-<unix_timestamp>-<victim-asn1_victim-asn2_...>=<attacker-asn1_attacker-asn2...>, with each asn being a valid AS number"
         raise ValidationError(err_str)
 
 def validate_defcon(as_list):
@@ -67,8 +70,8 @@ def validate_defcon(as_list):
         for asn in as_list_arr:
             assert is_valid_asn(asn)  
 
-    except AssertionError:
-        err_str = "Invalid DEFCON event ID. The right format for a DEFCON event is defcon-<unix_timestamp>-<victim-asn1_victim-asn2_...>"
+    except Exception as e:
+        err_str = "Invalid DEFCON event ID. The right format for a DEFCON event is defcon-<unix_timestamp>-<victim-asn1_victim-asn2_...>, with each asn being a valid AS number"
         raise ValidationError(err_str)
 
 def validate_edges(as_list):
@@ -77,8 +80,8 @@ def validate_edges(as_list):
         assert len(as_list_arr) == 2
         assert is_valid_asn(as_list_arr[0]) and is_valid_asn(as_list_arr[1])  
 
-    except AssertionError:
-        err_str = "Invalid edges event ID. The right format for a edges event is edges-<unix_timestamp>-<asn1_asn2>"
+    except Exception as e:
+        err_str = "Invalid edges event ID. The right format for a edges event is edges-<unix_timestamp>-<asn1_asn2>, with each asn being a valid AS number"
         raise ValidationError(err_str)
 
 
@@ -89,13 +92,14 @@ def validate_event_id(id):
         if len(id_split) != 3:
             # assuming every event is of the type
             # <eventtype-timestamp-aslist_details>
-
-            raise ValidationError()
+            err_str = "Invalid event ID"
+            raise ValidationError(err_str)
         
         event_type, timestamp, as_details = id_split
 
         if not is_valid_past_timestamp(timestamp):
-            raise ValidationError()
+            err_str = "Invalid timestamp. Timestamp must be of UNIX timestamp format from the past."
+            raise ValidationError(err_str)
         
         event_validation_strategy = {
             'moas': validate_moas,
@@ -109,3 +113,6 @@ def validate_event_id(id):
     except KeyError:
         err_str = "Unknown event type. Event type must be one of [ moas, submoas, defcon, edges ]"
         raise ValidationError(err_str)
+    
+    except Exception as e:
+        raise e
